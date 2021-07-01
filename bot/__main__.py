66998fa0b71270dc8276db8fa0c0029d12402779
Андрей Александрович <oficialmorozov@gmail.com>
2021-07-01 23:41:35 +0500

    Add possibilities to create a new cog

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 import logging
 
 from bot import root_path
@@ -19,9 +21,58 @@ def load_extensions():
     return [f"bot.cogs.{p.stem}" for p in ext_paths]
 
 
-bot = Bot()
+def run_bot():
+    bot = Bot()
 
-for ext in load_extensions():
-    bot.load_extension(ext)
+    for ext in load_extensions():
+        bot.load_extension(ext)
 
-bot.run()
+    bot.run()
+
+
+def create_cog(cog_name: str):
+    cogs_path = root_path / "bot/cogs"
+    cog_path = cogs_path / f"{cog_name.lower()}.py"
+
+    if cog_path.exists():
+        return
+
+    cog_code = f"""import discord
+
+from discord.ext import commands
+
+from .utils.base_cog import BaseCog
+
+
+class {cog_name}(BaseCog):
+    pass
+
+
+def setup(bot):
+    bot.add_cog({cog_name}(bot))"""
+
+    cog_path.write_text(cog_code, encoding="utf-8")
+    return cog_path
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--cog")
+    arg_parser.add_argument(
+        "--move", action="store_true", default=False, required=False
+    )
+    args = arg_parser.parse_args()
+
+    if args.cog:
+        cog_path = create_cog(args.cog)
+
+        if cog_path is None and not args.move:
+            return print(f"Cog {args.cog} is already created.")
+
+        if args.move:
+            return os.system(f"code {cog_path.absolute()}")
+
+    run_bot()
+
+
+main()
