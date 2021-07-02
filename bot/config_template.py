05a9ecf6f5a2afdeed378d1f03ec115e9ce6cb4b
Андрей Александрович <oficialmorozov@gmail.com>
2021-07-02 22:00:10 +0500

    Remove orm deps

--- bot/config_template.py
+++ bot/config_template.py
@@ -1,6 +1,3 @@
-from bot import root_path
-
-
 # Токен бота из https://discord.com/developers
 bot_token: str = ""
 
@@ -10,4 +7,3 @@ command_prefixes: list = ["!", "!!"]
 
 # Системные переменные
 debug: bool = False
-database_url: str = f"sqlite://{root_path / 'database.sqlite3'}"
