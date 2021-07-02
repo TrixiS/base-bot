e2371a742f4b0e0ffdab9cd08527842b28d131e0
Андрей Александрович <oficialmorozov@gmail.com>
2021-07-03 00:45:43 +0500

    Add orm cog

--- bot/config_template.py
+++ bot/config_template.py
@@ -1,3 +1,6 @@
+from bot import root_path
+
+
 # Токен бота из https://discord.com/developers
 bot_token: str = ""
 
@@ -7,3 +10,4 @@ command_prefixes: list = ["!", "!!"]
 
 # Системные переменные
 debug: bool = False
+database_url: str = f"sqlite://{root_path / 'database.sqlite3'}"
