cce0615ca8b34564e456619e6e8a9804405190ce
Андрей Александрович <oficialmorozov@gmail.com>
2021-07-03 14:29:30 +0500

    Add basic converters

---
+++ bot/cogs/utils/converters.py
@@ -0,0 +1,93 @@
+import textwrap
+
+from enum import Enum
+
+import discord
+
+from discord.ext import commands
+
+from bot.context import BotContext
+
+
+def safe_argument(argument: str) -> str:
+    placeholder = f"{argument[:3]}..."
+    return textwrap.shorten(argument, width=len(placeholder), placeholder=placeholder)
+
+
+class SafeBadArgument(commands.BadArgument):
+    def __init__(self, message: str, argument: str, *args, **fmt_kwargs):
+        super().__init__(
+            *args,
+            message=message.format(argument=safe_argument(argument), **fmt_kwargs),
+        )
+
+
+class NotAuthor(commands.MemberConverter):
+    async def convert(self, ctx: BotContext, argument: str) -> discord.Member:
+        member = await super().convert(ctx, argument)
+
+        if member == ctx.author:
+            raise commands.BadArgument(ctx.phrases.not_author)
+
+        return member
+
+
+class IntConverter(commands.Converter):
+    async def convert(self, ctx: BotContext, argument: str):
+        try:
+            return int(argument)
+        except ValueError:
+            raise SafeBadArgument(ctx.phrases.invalid_int, argument)
+
+
+class RangedIntConverter(IntConverter):
+    def __init__(
+        self,
+        start: int = None,
+        stop: int = None,
+        step: int = 1,
+        *,
+        argument_range: range = None,
+    ):
+        if argument_range is not None:
+            self.argument_range = argument_range
+            return
+
+        self.argument_range = range(start, stop + 1, step)
+
+    async def convert(self, ctx: BotContext, argument: str):
+        number = await super().convert(ctx, argument)
+
+        if number in self.argument_range:
+            return number
+
+        raise SafeBadArgument(
+            ctx.phrases.int_not_in_range,
+            argument,
+            range=range(self.argument_range.start, self.argument_range.stop - 1),
+        )
+
+
+class EnumConverter(commands.Converter):
+    def __init__(self, enum: Enum, convert_from_values: bool = False):
+        self.enum = enum
+        self.convert_from_values = convert_from_values
+
+    async def convert(self, ctx: BotContext, argument: str):
+        for name, value in self.enum.__members__.items():
+            if (
+                self.convert_from_values and argument == str(value.value)
+            ) or argument == name:
+                return value
+
+        values = (
+            [v.value for v in self.enum.__members__.values()]
+            if self.convert_from_values
+            else self.enum.__members__.keys()
+        )
+
+        raise SafeBadArgument(
+            ctx.phrases.invalid_enum_value,
+            argument,
+            values=" | ".join(f"`{v}`" for v in values),
+        )
--- bot/phrases/ru.py
+++ bot/phrases/ru.py
@@ -1,2 +1,13 @@
 # Фразы бота
 bot_started: str = "Бот {bot.user} успешно запущен"
+
+# Ошибки
+not_author: str = "Вы не можете использовать эту команду на себя"
+cant_use: str = "Вы не можете использовать эту команду"
+invalid_int: str = "Вы ввели неверное число - **{argument}**"
+int_not_in_range: str = (
+    "Число **{argument}** не находится в диапазоне **{range.start}-{range.stop}**"
+)
+invalid_enum_value: str = (
+    "Вместо **{argument}** необходимо указать одно из значений: {values}"
+)
