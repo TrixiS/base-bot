0d139c69930e7136ed0189fa071663cf2ff67d10
TrixiS <oficialmorozov@gmail.com>
2021-10-29 12:28:23 +0500

    Update files in separate functions

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ from bot.phrases import BotPhrases
 from pydantic import BaseModel
 
 
-def update_phrases_file_from_model(path: Path, model_cls: BaseModel):
+def update_json_file_from_model(path: Path, model_cls: BaseModel):
     if not path.exists():
         return
 
@@ -17,12 +17,19 @@ def update_phrases_file_from_model(path: Path, model_cls: BaseModel):
         f.write(json_string)
 
 
-def main():
+def update_config_files():
     for config_filename in BotConfig.__config_filenames__:
-        update_phrases_file_from_model(root_path / config_filename, BotConfig)
+        update_json_file_from_model(root_path / config_filename, BotConfig)
 
+
+def update_phrase_files():
     for phrases_path in (root_path / "phrases").glob("*.json"):
-        update_phrases_file_from_model(phrases_path, BotPhrases)
+        update_json_file_from_model(phrases_path, BotPhrases)
+
+
+def main():
+    update_config_files()
+    update_phrase_files()
 
 
 if __name__ == "__main__":
